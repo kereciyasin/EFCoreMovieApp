dfd915b79b92a6aebd082a7497e2b0bd33d922a3
Yasin Kereci <yke144907@stud.gibb.ch>
2025-05-08 17:08:15 +0200

    GenreEntityConfiguration

---
+++ EFCoreMovieApp/Infra/EntityTypeConfigurations/GenreEntityConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using EFCoreMovieApp.Infra.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace EFCoreMovieApp.Infra.EntityTypeConfigurations
+{
+    public class GenreEntityConfiguration : BaseEntityTypeConfiguration<GenreEntity>
+    {
+        public override void Configure(EntityTypeBuilder<GenreEntity> builder)
+        {
+
+            builder.ToTable(name: "Genres", schema: "ef");
+            builder.Property(e => e.Name)
+                .HasColumnName("Name")
+                .IsRequired()
+                .HasMaxLength(100);
+
+
+            base.Configure(builder);
+        }
+    }
+
+}
