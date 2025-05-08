c516823359775d0150932c17650b6f54f7f4e4af
Yasin Kereci <yke144907@stud.gibb.ch>
2025-05-08 17:16:45 +0200

    DirectorEntityConfiguration ADDED

---
+++ EFCoreMovieApp/Infra/EntityTypeConfigurations/DirectorEntityConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using EFCoreMovieApp.Infra.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreMovieApp.Infra.EntityTypeConfigurations
+{
+    public class DirectorEntityConfiguration : BaseEntityTypeConfiguration<DirectorEntity>
+    {
+        public override void Configure(EntityTypeBuilder<DirectorEntity> builder)
+        {
+            builder.ToTable(name: "Directors", schema: "ef");
+
+
+            // TODO: Movies relation
+            // One to many relation with MovieEntity
+
+            builder.HasMany(d => d.Movies)
+                .WithOne(m => m.Director)
+                .HasForeignKey(m => m.DirectorId);
+
+            builder.Property(e => e.FirstName)
+                .HasColumnName("FirstName")
+                .IsRequired()
+                .HasMaxLength(100);
+            builder.Property(e => e.LastName)
+                 .HasColumnName("LastName")
+                .IsRequired()
+                .HasMaxLength(100);
+            base.Configure(builder);
+        }
+    }
+}
